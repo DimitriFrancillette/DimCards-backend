b98517f8c9da6a8192fb4f99991f3907e2c380b9
Dimitri <dfrancillette@gmail.com>
2024-01-18 17:29:42 +0100

    connection to database done

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
+require('./models/connection');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,9 +12,7 @@ console.log("it's working")
 
 var app = express();
 
-const cors = require('cors');
 app.use(cors());
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
