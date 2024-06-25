69623435181b1c979fa29b4e6646854727f07579
Dimitri <dfrancillette@gmail.com>
2024-06-25 16:50:02 +0200

    changes in the catch userUpdate function and responses of deleteUser function

--- controller/userController.js
+++ controller/userController.js
@@ -83,6 +83,7 @@ const signInUser = async (req, res) => {
 };
 
 const updateUser = async (req, res) => {
+  console.log(req.body);
   if (!checkBody(req.body, ['email'])) {
     res.status(400).json({ error: 'User email is missing' });
     return;
@@ -99,6 +100,7 @@ const updateUser = async (req, res) => {
     if (req.body.password) {
       toUpdate.password = bcrypt.hashSync(req.body.password, 10);
     }
+    console.log('2update', toUpdate);
 
     await User.findOneAndUpdate({ email: req.body.email }, toUpdate, {
       new: true,
@@ -113,16 +115,19 @@ const updateUser = async (req, res) => {
       });
     });
   } catch (error) {
+    if (error.code === 11000) {
+      return res.status(400).json({ message: 'Username already exist' });
+    }
     return res.status(500).json({
-      message: 'Cannot update, internal server error',
-      error: error,
+      message: 'Cannot create user, internal server error',
+      error,
     });
   }
 };
 
 const deleteUser = async (req, res) => {
   if (!checkBody(req.body, ['email'])) {
-    res.status(400).json({ error: 'Missing or empty fields' });
+    res.status(400).json({ message: 'Missing or empty fields' });
     return;
   }
   try {
@@ -130,7 +135,7 @@ const deleteUser = async (req, res) => {
     if (deletetedUser.deletedCount > 0) {
       return res.status(200).json({ message: 'User deleted' });
     } else {
-      return res.status(404).json({ error: 'User not found' });
+      return res.status(404).json({ message: 'User not found' });
     }
   } catch (error) {
     return res.status(500).json({
